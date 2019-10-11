b3b09c73e3a6bb1101dcbb252cb59d066c7dc7d8
Jeremias Platero <lightjedi5@byui.edu>
2019-10-11 17:46:00 -0600

    Added my hello

--- src/helloFriends/helloJerry.java
+++ src/helloFriends/helloJerry.java
@@ -4,5 +4,6 @@ public class helloJerry {
     public static void main(String [] args)
     {
         System.out.println("Hello Jerry-San!");
+        System.out.println("Ohio Goziama, Reynolds-San!");
     }
 }
